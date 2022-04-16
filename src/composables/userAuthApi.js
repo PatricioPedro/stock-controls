1c9d54737ee123ac1eeb3ecd8f0e15e5d5982a73
Patricio Wilder Barros Pedro <patriciopedro@MBP-de-Patricio.home>
2022-04-16 23:31:30 +0100

    UserAuth Composable

---
+++ src/composables/userAuthApi.js
@@ -0,0 +1,70 @@
+
+import useSupabase from 'boot/supabase'
+
+import { ref } from 'vue'
+
+const user = ref(null)
+
+export default function useAuthUser() {
+
+    const { supabase } = useSupabase()
+
+    const login = async ({ email, password }) => {
+        const { user, error } = await supabase.auth.signIn({ email, password })
+        if (error) throw error
+        return user
+    }
+
+    const loginWithProvider = async (provider) => {
+
+        const { user, error } = await supabase.auth.signIn({provider})
+        if (error) throw error
+        return user
+
+    }
+    const register = async ({ email, password, ...meta }) => {
+        const { user, error } = await supabase.auth.signUp({ email, password }, {
+            data: meta,
+            redirectTo: `${window.location.origin}/me?fromEmail=registrationConfirmation`
+        })
+
+        if (error) throw error
+        return user
+    }
+
+    const isLoggedIn = () => {
+        return !!user.value
+    }
+
+    const logout = async () => {
+
+        const { error } = await supabase.auth.signOut()
+
+        if (error) throw error
+    }
+
+    const sendPasswordRestEmail = async (email) => {
+
+        const {user, error} = await supabase.auth.api.resetPasswordForEmail(email)
+
+        if (error) throw error
+        return user
+    }
+
+    const update = async (data) => {
+        const {user, error} = await supabase.auth.update(data)
+        if (error) throw error
+        return user
+    }
+
+    return {
+        user,
+        login,
+        logout,
+        isLoggedIn,
+        update,
+        register,
+        sendPasswordRestEmail,
+        loginWithProvider
+    }
+}
